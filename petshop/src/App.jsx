6b11466bd539d417b5cd5b41c3d6cd28e2541b5f
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2021-09-02 14:19:23 -0300

    instalando dependencias

---
+++ petshop/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './assets/css/base/base.css'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+
+          <Route exact path='/Sobre'>
+            <Sobre />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  )
+}
+
+export default App
